105a0cbbebe2799f0990987efda7f3508e0497ad
sierra-95 <michaelmachohi@gmail.com>
2026-02-22 23:03:41 +0300

    edit and update page working well

--- src/lib/Navigator/main.svelte
+++ src/lib/Navigator/main.svelte
@@ -1,18 +1,57 @@
 <script>
+    import { browser } from '$app/environment';
     import { page } from '$app/stores';
     import { sections } from '$lib';
     import { flattenSections } from './flatten';
 
+    const prefix = "src/routes";
+    const baseURL = "https://github.com/sierra-95/svelte-scaffold-docs/tree/main/"
+
+    let currentPath = $state();
+    let previous = $state();
+    let next = $state();
+    let pageURL = $state('');
+    let lastUpdated = $state('');
+
     const allPages = flattenSections(sections);
+    $effect(() => {
+        if(browser){
+            currentPath = $page.url.pathname;
+            const currentIndex = allPages.findIndex(p => p.path === currentPath);
+            previous = currentIndex > 0 ? allPages[currentIndex - 1] : null;
+            next = currentIndex < allPages.length - 1
+                ? allPages[currentIndex + 1]
+                : null;
+            if(currentPath){
+                pageURL = prefix + currentPath + '/%2Bpage.svelte';
+                load();
+            }
+        }
+    });
 
-    $: currentPath = $page.url.pathname;
+    async function load() {
+        const endpoint = `https://api.github.com/repos/sierra-95/svelte-scaffold-docs/commits?path=${pageURL}&per_page=1`;
+        try {
+            const res = await fetch(endpoint, {
+                headers: {
+                    'Accept': 'application/vnd.github.v3+json'
+                }
+            });
 
-    $: currentIndex = allPages.findIndex(p => p.path === currentPath);
+            if (!res.ok) {
+                console.error('GitHub API request failed:', res.status, res.statusText);
+                lastUpdated = '-';
+                return;
+            }
+            const data = await res.json();
+            lastUpdated = data?.[0]?.commit?.author?.date ?? '-';
 
-    $: previous = currentIndex > 0 ? allPages[currentIndex - 1] : null;
-    $: next = currentIndex < allPages.length - 1
-        ? allPages[currentIndex + 1]
-        : null;
+        } catch (err) {
+            console.error('Error fetching last updated date:', err);
+        }
+    }
+
+
 </script>
 <style>
     #sierra-navigation{
@@ -20,10 +59,10 @@
         justify-content: space-between;
         margin-top: 3rem;
     }
-    #sierra-navigation a{
+    #sierra-navigation a, #sierra-github-page-edit a{
         transition: color 0.1s ease-in;
     }
-    #sierra-navigation a:hover{
+    #sierra-navigation a:hover, #sierra-github-page-edit a:hover{
         color: var(--primary-bg);
     }
     #sierra-navigation h3{
@@ -43,24 +82,33 @@
         }
     }
 </style>
-<main id="sierra-navigation">
 
-    {#if previous}
-        <div>
-            <h3 class="text-previous">Previous</h3>
-            <a href={previous.path}>
-                {previous.trail.join(' → ')}
-            </a>
+<section class="space-y-4">
+    <div id="sierra-navigation">
+        {#if previous}
+            <div>
+                <h3 class="text-previous">Previous</h3>
+                <a href={previous.path}>
+                    {previous.trail.join(' → ')}
+                </a>
+            </div>
+        {/if}
+
+        {#if next}
+            <div>
+                <h3>Next</h3>
+                <a href={next.path}>
+                    {next.trail.join(' → ')}
+                </a>
+            </div>
+        {/if}
+
         </div>
-    {/if}
 
-    {#if next}
-        <div>
-            <h3>Next</h3>
-            <a href={next.path}>
-                {next.trail.join(' → ')}
-            </a>
-        </div>
-    {/if}
-
-</main>
+    <div id="sierra-github-page-edit" class="flex justify-between text-(--text-secondary) text-sm">
+        <a href={baseURL + pageURL} target="_blank" rel="noreferrer" ><i class="fa-solid fa-pen mr-2"></i>Edit Page</a>
+        <h3>Last Updated:
+            {lastUpdated? new Date(lastUpdated).toLocaleDateString(): '-'}
+        </h3>
+    </div>
+</section>
