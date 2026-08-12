a3f48d83930f666ae10a38da65600e0471a54782
sierra-95 <michaelmachohi@gmail.com>
2026-08-12 15:02:23 +0300

    featurres complete

--- src/routes/stores/+page.svelte
+++ src/routes/stores/+page.svelte
@@ -1,19 +1,24 @@
 <script>
     import {isLoading, Button} from '@sierra-95/svelte-scaffold';
-    import {RenderCode, routes, sectionIds} from '$lib';
+    import {RenderCode} from '$lib';
+	import {routes} from '$lib/assets/company';
 
     function toggleLoading() {
         isLoading.update((value) => !value);
     }
 </script>
 
-<main class="space-y-4">
+{#snippet layout_link()}
+	<a href={routes.modules.layout.children.overview.path} class="note">Layout component</a> 
+{/snippet}
+
+<main class="space-y-6">
 	<title>General Stores</title>
 	<h1 class="sierra-docs-h1">Store is closed</h1>
 
-	<ol class="sierra-docs-ol">
+	<ol class="sierra-docs-ol space-y-4">
 	
-		<section id={sectionIds.stores_utils.stores.is_mobile} class="space-y-4 mb-10">
+		<section id={routes.stores.ids.device_width} data-title="Device Width" class="space-y-4">
 			<li>Device Width</li>
 			<h3>isMobile (&lt;480px), isTablet ( 480px &gt & &lt; 768px), isDesktop (&gt1024px)</h3>
 			<RenderCode
@@ -30,12 +35,10 @@
 			`}/>
 		</section>
 
-		<section id={sectionIds.stores_utils.stores.is_loading} class="space-y-4 mb-10">
+		<section id={routes.stores.ids.isLoading} data-title="App Loading" class="space-y-4">
 			<li>isLoading</li>
-			<h3>
-			Implemented internally in
-			<a class="note" href={routes.modules.layout.children.overview}>Layout</a>,
-			so it can be used, for example, in form submissions.
+			<h3>Implemented internally in {@render layout_link?.()}, so it can be used, for example, in form submissions.
+			It is also triggered during beforeNavigate and afterNavigate events, which can be used to show a loading indicator during page transitions.
 			</h3>
 			<Button onclick={toggleLoading}>Click here</Button>
 			<RenderCode
@@ -51,7 +54,7 @@
 			`}/>
 		</section>
 	
-		<section id={sectionIds.stores_utils.stores.is_logged_in} class="space-y-4 mb-10">
+		<section id={routes.stores.ids.isLoggedIn} data-title="App Authentication" class="space-y-4">
 			<li>isLoggedIn</li>
 			<h3>Default value is <code>null</code></h3>
 			<RenderCode
@@ -71,10 +74,10 @@
 			`}/>
 		</section>
 
-		<section id={sectionIds.stores_utils.stores.user_store} class="space-y-4 mb-10">
+		<section id={routes.stores.ids.user_store} data-title="User Store" class="space-y-4">
 			<li>User Store</li>
-			<h3>This store can be used components to manage user-related data including the  
-				<a href={`${routes.core.components.children.menus}#${sectionIds.components.menu.user_menu}`} class="note">This example</a> 
+			<h3>This store can be used components to manage user-related data like 
+				<a href={`${routes.core.components.children.menus.path}#${routes.core.components.children.menus.ids.user_menu}`} class="note">This example</a> 
 			</h3>
 			<RenderCode
 				lang="svelte"
@@ -104,13 +107,10 @@
 			`}/>
 		</section>
 
-		<section id={sectionIds.stores_utils.stores.theme_store} class="space-y-4">
+		<section id={routes.stores.ids.theme_store} data-title="Theme Store" class="space-y-4">
 			<li>Theme Store</li>
-			<h3>The theme store is tightly integrated with the 
-				<a href={routes.modules.layout.children.overview} class="note">Layout component</a> 
-				and internal body styling.
-			</h3>
-			<h3>It is also directly manipulated by the <a href={routes.core.components.children.buttons + '#' + sectionIds.components.buttons.theme_button} class="note">Theme Button</a>.</h3>
+			<h3>The theme store is tightly integrated with the {@render layout_link?.()} and internal body styling.</h3>
+			<h3>It is also directly manipulated by the <a href={routes.core.components.children.buttons.path + '#' + routes.core.components.children.buttons.ids.theme_button} class="note">Theme Button</a>.</h3>
 			<RenderCode
 				lang="svelte"
 				code={`
