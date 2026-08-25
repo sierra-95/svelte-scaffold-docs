9103d8fbda080403d7ab80e030c2d34837ca153a
sierra-95 <michaelmachohi@gmail.com>
2026-08-25 11:18:00 +0300

    overflow x in utils fixed

--- src/lib/assets/company.ts
+++ src/lib/assets/company.ts
@@ -2,7 +2,6 @@ export { sections } from './conf/sections';
 export { routes } from './conf/routes';
 
 const base_url="https://files.michaelmachohi.com/logos/";
-export const logo = `${base_url}michaelmachohi.dark.png`;
 export const favicon = `${base_url}faviconV2/`;
 
 
